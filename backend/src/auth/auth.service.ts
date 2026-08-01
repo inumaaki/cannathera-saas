5df933944da1e187d6c868d7dea61cfa949ce9cc
Muhammad Hassan Raza <mhassanraza117@gmail.com>
2026-08-01 16:07:35 +0500

    tmp 2fa off

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -60,7 +60,7 @@ export class AuthService {
   constructor(
     private readonly prisma: PrismaService,
     private readonly jwt: JwtService,
-  ) {}
+  ) { }
 
   // ---------------------------------------------------------------- register
   async register(dto: RegisterDto, ip?: string) {
@@ -183,11 +183,11 @@ export class AuthService {
                 type: OrgType.PRACTICE,
                 branding: data
                   ? {
-                      lanr: data.lanr,
-                      bsnr: data.bsnr,
-                      specialty: data.specialty,
-                      phone: data.phone,
-                    }
+                    lanr: data.lanr,
+                    bsnr: data.bsnr,
+                    specialty: data.specialty,
+                    phone: data.phone,
+                  }
                   : undefined,
               },
             });
@@ -221,11 +221,11 @@ export class AuthService {
                 type: OrgType.PHARMACY,
                 branding: data
                   ? {
-                      contactPerson: data.contactPerson,
-                      address: data.address,
-                      phone: data.phone,
-                      idf: data.idf,
-                    }
+                    contactPerson: data.contactPerson,
+                    address: data.address,
+                    phone: data.phone,
+                    idf: data.idf,
+                  }
                   : undefined,
               },
             });
@@ -248,10 +248,10 @@ export class AuthService {
                 type: OrgType.ENTERPRISE,
                 branding: data
                   ? {
-                      contactPerson: data.contactPerson,
-                      partnerType: data.partnerType,
-                      phone: data.phone,
-                    }
+                    contactPerson: data.contactPerson,
+                    partnerType: data.partnerType,
+                    phone: data.phone,
+                  }
                   : undefined,
               },
             });
@@ -424,7 +424,8 @@ export class AuthService {
     const policy = await this.policyFor(user.id);
 
     // 2FA skipped for ADMIN role because they can't access the email
-    const skip2fa = !policy.mandatory2fa || user.role === 'ADMIN';
+    // TEMPORARY FOR TESTING: Force 2FA to be skipped for all accounts
+    const skip2fa = true; // !policy.mandatory2fa || user.role === 'ADMIN';
 
     if (skip2fa) {
       await this.prisma.auditLog.create({
